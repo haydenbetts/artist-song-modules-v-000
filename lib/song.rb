b3d169aa51aaef006103ab823fc6629d0dbf6ee9
Hayden Betts <haydenbetts@gmail.com>
2018-06-28 22:42:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,11 +3,15 @@ require 'pry'
 class Song
   extend Memorable, Findable
   include Paramable
-  
+
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
+
+  def self.all
+    @@songs
+  end
 
   def initialize
     @@songs << self
