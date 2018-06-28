53eb66ef247f289bdb066d2364b5b2aa2646caa6
Hayden Betts <haydenbetts@gmail.com>
2018-06-28 22:30:33 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,14 +2,11 @@ require 'pry'
 
 class Artist
   extend Memorable
+
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
-
-  def self.all
-    @@artists
-  end
 
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
